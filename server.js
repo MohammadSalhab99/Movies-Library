c18c40d14f235c13a8cc5c3fddb462ab04478ba3
MohammadSalhab99 <mohammadsalhab8@gmail.com>
2022-02-12 00:42:59 +0200

    Task completed

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 app.get('/', helloWorldHandler);
-app.get('/drinks', drinks)
-app.get('/favorite', wolcomingHandler)
-app.get('/trending', trending)
-app.get('/search', searchf)
-app.post('/addMovie', addMovieF)
-app.get('/getMovies', getMoviesF)
-app.get('*', serverErorr)
-app.get('#', pageNotFoundErorr)
+app.get('/drinks', drinks);
+app.get('/favorite', wolcomingHandler);
+app.get('/trending', trending);
+app.get('/search', searchf);
+app.post('/addMovie', addMovieF);
+app.get('/getMovies', getMoviesF);
+app.put('/updateMovie/:id', updateMovieF);
+app.delete('/deleteMovie/:id', deleteMovieF);
+app.get('*', serverErorr);
+app.get('#', pageNotFoundErorr);
 
 
-const server = express();
-server.use(express.json());
-server.use(cors());
 //let url = `https://api.themoviedb.org/3/trending/all/week?api_key=37ddc7081e348bf246a42f3be2b3dfd0&language=en-US`
 
 let url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`;
@@ -76,18 +76,46 @@ function searchf(req, res) {
 }
 
 function addMovieF(req, res) {
-    const movie = req.body;
+    let movie = req.body;
     console.log(movie);
     let sql = `INSERT INTO favMovies(strDrink,strDrinkThumb,idDrink) VALUES ($1,$2,$3) RETURNING *;`
     let values = [movie.strDrink, movie.strDrinkThumb, movie.idDrink];
     client.query(sql, values).then(data => {
         res.status(200).json(data.rows);
     }).catch(error => {
-        errorHandler(error, req, res)
+        serverErorr(error, req, res)
     });
 }
 
 function getMoviesF(req, res) {
+    let sql = `SELECT * FROM favMovies;`;
+    client.query(sql).then(data => {
+        res.status(200).json(data.rows);
+    }).catch(error => {
+        serverErorr(error, req, res)
+    });
+
+}
+
+function updateMovieF(req, res) {
+    const id = req.params.id;
+    const movie = req.body;
+    const sql = `UPDATE favMovies SET strDrink =$1, strDrinkThumb = $2 WHERE idDrink=$3 RETURNING *;`;
+    let values = [movie.strDrink, movie.strDrinkThumb, id];
+    client.query(sql, values).then(data => {
+        res.status(200).json(data.rows);
+    }).catch((err) => { console.log(err.message) })
+}
+
+function deleteMovieF(req, res) {
+    const id = req.params.id;
+    const sql = `DELETE FROM favMovies WHERE idDrink=${id};`
+
+    client.query(sql).then(() => {
+        res.status(200).send("The Drink has been deleted");
+    }).catch(error => {
+        serverErorr(error, req, res)
+    });
 
 }
 
@@ -98,8 +126,10 @@ function serverErorr(req, res) {
 function pageNotFoundErorr(req, res) {
     return res.status(404).send("Page Not found")
 }
+client.connect().then(() => {
+    app.listen(3000, () => {
 
-app.listen(3000, () => {
+        console.log('listening to port 3000')
+    })
 
-    console.log('listening to port 3000')
 })
